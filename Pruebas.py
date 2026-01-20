92e5470de33fd14f4ce5b8103382d03eb97386c0
Cubas-G <fredyscubas00@gmail.com>
2026-01-20 05:52:00 +0100

    Cuarto commit Tarea

---
+++ Pruebas.py
@@ -0,0 +1,61 @@
+#programacion=input()
+#print(len(programacion))
+
+
+
+#colores = {'rojo': '#ff0000', 'verde': '#00ff00'}
+#print(colores['verde'])
+
+#nums = [10, 20, 30]
+#nums[1] = 99
+#print(nums)
+
+#letras = ['a','b', 'c','d', 'e', ]
+#print(letras[:3])
+#print(letras[-2:])
+
+#a = float(input())
+#b = int(input())
+#print(int(a + b))
+
+""" estado = {'puntos': 10, 'vidas': 2}
+
+estado['puntos'] += 5   # Sumo 5 a los puntos
+estado['vidas'] = 3     # Cambio las vidas a 3
+
+print(estado) """
+
+
+
+
+""" a = int(input())
+b = int(input())
+
+print(a + b)
+print(a * b) """
+
+
+
+
+
+""" valores = [3, 7, 0, 7]
+
+print(valores[1])          # Segundo elemento
+print(valores[0] + valores[-1])  # Suma del primero y el último """
+
+""" nombres = ['Ana', 'Luis', 'Eva']
+notas = {'Ana': 8.5, 'Luis': 7.0, 'Eva': 9.0}
+
+resultado = (
+    nombres[0] + "-" + str(notas[nombres[0]]) + " , " +
+    nombres[1] + "-" + str(notas[nombres[1]]) + " , " +
+    nombres[2] + "-" + str(notas[nombres[2]])
+)
+
+print(resultado) """
+
+
+nombre = input()
+edad = int(input())
+
+print("Nombre: " + nombre + " (Edad: " + str(edad) + ")")
